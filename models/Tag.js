4c96e7aa808b247f4efe59487f9498b6b482c8b3
luxardOri <127153726+luxardOri@users.noreply.github.com>
2023-07-12 13:29:08 -0700

    adding in tag model

--- models/Tag.js
+++ models/Tag.js
@@ -6,11 +6,20 @@ class Tag extends Model {}
 
 Tag.init(
   {
-    // define columns
+    tag_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    tag_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
   },
   {
     sequelize,
-    timestamps: false,
+    timestamps: true,
     freezeTableName: true,
     underscored: true,
     modelName: 'tag',
