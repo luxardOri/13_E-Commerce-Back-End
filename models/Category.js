8ab9ddc02f601dfeb218029e3ca0718363ac2dae
luxardOri <127153726+luxardOri@users.noreply.github.com>
2023-07-12 13:20:59 -0700

    adding in category model

--- models/Category.js
+++ models/Category.js
@@ -6,11 +6,20 @@ class Category extends Model {}
 
 Category.init(
   {
-    // define columns
+    category_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
   },
   {
     sequelize,
-    timestamps: false,
+    timestamps: true,
     freezeTableName: true,
     underscored: true,
     modelName: 'category',
