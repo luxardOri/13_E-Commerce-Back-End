0ff22eba41d114ea4f23c9e42f7d5de362f0d079
luxardOri <127153726+luxardOri@users.noreply.github.com>
2023-08-06 16:43:58 -0700

    added in delete route for category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -51,8 +51,17 @@ router.put("/:id", async (req, res) => {
   }
 });
 
-router.delete("/:id", (req, res) => {
+router.delete("/:id", async (req, res) => {
   // delete a category by its `id` value
+  try {
+    const categories = await Category.destroy({
+      where: { category_id: req.params.id }
+    });
+
+    res.status(200).json(categories);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
