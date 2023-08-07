fbffa42cde4105f08c19200901aff3cc884ff478
luxardOri <127153726+luxardOri@users.noreply.github.com>
2023-08-06 19:32:00 -0700

    added in delete route for tags

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -51,8 +51,17 @@ router.put('/:id', async (req, res) => {
   }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
+  try {
+    const tags = await Tag.destroy({
+      where: { tag_id: req.params.id }
+    });
+
+    res.status(200).json(tags);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
