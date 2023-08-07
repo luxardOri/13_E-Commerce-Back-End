c3591300c2856f0a05925433d49f6f770d8085fb
luxardOri <127153726+luxardOri@users.noreply.github.com>
2023-08-06 19:20:11 -0700

    added in delete route for products

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -106,10 +106,24 @@ router.put("/:id", (req, res) => {
     });
 });
 
-router.delete("/:id", (req, res) => {
+router.delete("/:id", async (req, res) => {
   // delete one product by its `id` value
-  
+  try {
+    const products = await Product.destroy({
+      where: {
+        product_id: req.params.id
+      }
+    });
 
+    if (!products) {
+      res.status(404).json({ message: 'No products found with this id!' });
+      return;
+    }
+
+    res.status(200).json(products);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
