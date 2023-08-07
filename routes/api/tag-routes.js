73e7d4b02c493eafa503fe4990f36a09bc4805fe
luxardOri <127153726+luxardOri@users.noreply.github.com>
2023-08-06 19:27:45 -0700

    adding in post tag route

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -29,8 +29,14 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
+  try {
+    const tags = await Tag.create(req.body);
+    res.status(200).json(tags);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
