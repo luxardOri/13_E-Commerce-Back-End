4905feb7a924b7ae46d110f3cf8f663b8e92fae5
luxardOri <127153726+luxardOri@users.noreply.github.com>
2023-08-06 19:30:12 -0700

    added in update tag by id route

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -39,8 +39,16 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const tags = await Tag.update(req.body,{
+      where: { tag_id: req.params.id }
+    })
+    res.status(200).json(tags);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.delete('/:id', (req, res) => {
